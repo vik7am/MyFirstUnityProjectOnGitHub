4a2e9a0809edd211306031e55b53e1a3d98bbc1e
Vikrant Kataria <vik7am@gmail.com>
2022-06-18 17:15:00 +0530

    Added Scene Loader Script
    Created Scene loader script for buttons to load the next scene.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Button button;
+    public string sceneName;
+
     void Start()
     {
-        
+        button.onClick.AddListener(OnButtonClick);
     }
-
-    // Update is called once per frame
-    void Update()
+    void OnButtonClick()
     {
-        
+        SceneManager.LoadScene(sceneName);
     }
 }
